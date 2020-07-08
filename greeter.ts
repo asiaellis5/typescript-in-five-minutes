e55e859784e3558cdc20de05e57b086e8994f37d
Asia <asiaellis5@gmail.com>
2020-07-08 17:21:44 +0100

    added a student class

--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,12 @@
+class Student {
+  fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+
+
 interface Person {
   firstName: string;
   lastName: string;
